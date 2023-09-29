1059d28f2279d0b7244a94da715f4a8411e8f3ab
Aaron Tekle <108105300+AaTekle@users.noreply.github.com>
2023-09-29 10:15:27 -0400

    Update and rename BibleVerse_Generator.js to script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+     // Function to refresh the verse
+        function refreshVerse() {
+            // Reload the iframe or perform any other refresh action here
+            location.reload();
+        }
