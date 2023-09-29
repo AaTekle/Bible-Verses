41d84ba7b94d18b0d57f5783498cdf14051f4255
Aaron Tekle <108105300+AaTekle@users.noreply.github.com>
2023-09-29 18:44:47 -0400

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-     // Function to refresh the verse
-        function refreshVerse() {
-            // Reload the iframe or perform any other refresh action here
-            location.reload();
-        }
